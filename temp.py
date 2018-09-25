7c789ef201b5800300359f192ff604ffcc9a8339
Samuzzal Choudhury <samuzzal@redhat.com>
2018-09-25 08:47:29 +0530

    cleanup

--- temp.py
+++
@@ -1,5 +0,0 @@
-qstring = ("g.V().has('pecosystem','" + "maven" + "').has('pname','" + "io.vertx:vertx-core" + "').has('version','" + "3.4.1" + "').")
-qstring += ("as('version').in('has_version').as('package')." + "select('version','package').by(valueMap());")
-payload = {'gremlin': qstring}
-
-print (payload)
